60f07e392a4c44dc9a62f1d3b174f7b3e1bc7fb8
valencra <valencra@mcmaster.ca>
2018-01-28 22:43:58 -0500

    Added API endpoints for adding new courses, getting all courses, and getting courses by ID

--- src/main/java/com/teamtreehouse/courses/dao/Sql2oCourseDao.java
+++ src/main/java/com/teamtreehouse/courses/dao/Sql2oCourseDao.java
@@ -38,4 +38,13 @@ public class Sql2oCourseDao implements CourseDao {
           .executeAndFetch(Course.class);
     }
   }
+
+  @Override
+  public Course findById(int id) {
+    try(Connection connection = sql2o.open()) {
+      return connection.createQuery("SELECT * FROM courses WHERE id = :id")
+          .addParameter("id", id)
+          .executeAndFetchFirst(Course.class);
+    }
+  }
 }
