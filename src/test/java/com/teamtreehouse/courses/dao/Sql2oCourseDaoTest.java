fc6593489638d35d3b65b059b2bcbccef76ac5d6
valencra <valencra@mcmaster.ca>
2018-01-28 22:47:30 -0500

    Added test to ensure existing courses can be found by ID

--- src/test/java/com/teamtreehouse/courses/dao/Sql2oCourseDaoTest.java
+++ src/test/java/com/teamtreehouse/courses/dao/Sql2oCourseDaoTest.java
@@ -32,7 +32,7 @@ public class Sql2oCourseDaoTest {
 
   @Test
   public void addingCourseSetsId() throws Exception {
-    Course course = new Course("Test", "http://test.com");
+    Course course = newTestCourse();
     int originalCourseId = course.getId();
 
     dao.add(course);
@@ -42,7 +42,7 @@ public class Sql2oCourseDaoTest {
 
   @Test
   public void addedCoursesAreReturnedFromFindAll() throws Exception {
-    Course course = new Course("Test", "http://test.com");
+    Course course = newTestCourse();
 
     dao.add(course);
 
@@ -52,5 +52,19 @@ public class Sql2oCourseDaoTest {
   @Test
   public void noCoursesReturnsEmptyList() throws Exception {
     assertEquals(0, dao.findAll().size());
+  }
+
+  @Test
+  public void existingCoursesCanBeFoundById() throws Exception {
+    Course course = newTestCourse();
+    dao.add(course);
+
+    Course foundCourse = dao.findById(course.getId());
+
+    assertEquals(course, foundCourse);
+  }
+
+  private Course newTestCourse() {
+    return new Course("Test", "http://test.com");
   }
 }
